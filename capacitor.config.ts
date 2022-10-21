758262732220e03555da45e0675350524a705b26
sasangachathumal <chathumal.sasanga02@gmail.com>
2022-10-21 23:50:28 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-ng-auth-ui-tutorial',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
